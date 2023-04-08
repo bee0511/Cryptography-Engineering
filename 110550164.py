3fb41a0d933a10331484d0e03bc8d1c926dd8fad
bee0511 <bee000092@gmail.com>
2023-04-08 23:11:59 +0800

    problem modified no Galois

--- 110550164.py
+++ 110550164.py
@@ -1,26 +1,65 @@
-import numpy as np
-import galois
 def Berlekamp_Massey_algorithm(sequence):
-    N = len(sequence) # get the length of the input sequence
-    a = galois.Poly((np.flip(np.array([0] * N + [1]))), field=GF)
-    b = galois.Poly(np.array(sequence), field=GF)
-    x, prevx = galois.Poly(np.array([0]), field=GF), galois.Poly(np.array([1]), field=GF)
-    y, prevy = galois.Poly(np.array([1]), field=GF), galois.Poly(np.array([0]), field=GF)
-    while(len(prevy) < len(b)):
-        q, r = a // b, a % b
-        a, b = b, r
-        x, prevx = prevx - q * x, x
-        y, prevy = prevy - q * y, y
-    return prevy, prevy.nonzero_degrees[0]
+    f_min = [0] 
+    l = [0] 
+    for i in range(len(sequence)):
+        d = 0
+        for j in f_min:
+            d += sequence[i + j - max(l)]
+        d = d % 2
+        if d == 0:
+            l.append(l[i])
+        else: 
+            if the_same(l): 
+                n = i
+                fn = f_min.copy() 
+                f_min.append(i + 1)
+                l.append(i + 1)
+            else: 
+                if max(f_min) > max(fn):
+                    m = n
+                    fm = fn.copy()
+                n = i
+                fn = f_min.copy()
+                if m - l[m] >= n - l[n]:
+                    f_min += [j + (m - l[m] - n + l[n]) for j in fm] 
+                else: 
+                    f_min = [j - (m - l[m] - n + l[n]) for j in f_min] + fm
+                l.append(max(f_min))
+    f_min = condense(f_min)
+    return f_min
+
+def condense(f_min): # make f into GF(2)
+    f = list(set(f_min))
+    for i in f_min:
+        if f_min.count(i) % 2 == 0:
+            if i in f:
+                f.remove(i)
+    f = sorted(f, reverse=True)
+    return f
+    
+def the_same(l): 
+    for i in range(len(l) - 1):
+        if l[i] != l[i + 1]:
+            return False
+    return True
+
+def print_f(f_min): 
+    result = ''
+    for i in f_min:
+        if i == 0:
+            result += '1'
+        else:
+            result += 'x^' + str(i)
+        if i != f_min[-1]:
+            result += '+'
+    return result
     
 if __name__ == '__main__':
     
-    GF = galois.GF2
     seq = ( 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1)
     # seq = (0, 0, 0, 1, 0, 0, 1, 1) 
-    # seq = [1, 0, 1, 1, 0, 0, 0, 1]
-    (poly, span) = Berlekamp_Massey_algorithm(seq)
-
+    # seq = (1, 0, 1, 1, 0, 0, 0, 1)
+    poly = Berlekamp_Massey_algorithm(seq)
     print ('The input sequence is', seq)
-    print ('Its characteristic polynomial is', poly)
-    print ('and linear span is', span)
+    print ('Its characteristic polynomial is', print_f(poly))
+    print ('and linear span is', str(max(poly)))
